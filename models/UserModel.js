8cb019e566927d18bb6f0c5e0e77d487ad0544b3
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-09-24 16:49:56 +0500

    User model created

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,26 +1,36 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Username is required"],
-  },
-  penFriends: [
-    {
-      name: {
-        type: String,
-        required: [true, "Friend name should be exist!"],
-      },
-      messages: [
-        {
-          message: { type: String },
-          from: { type: String },
-          to: { type: String },
-        },
-      ],
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Username is required"],
     },
-  ],
-});
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "Email is required"],
+      index: true,
+      validate: [isEmail, "Invalid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    newMessages: {
+      type: Object,
+      default: {},
+    },
+    status: {
+      type: String,
+      default: "online",
+    },
+  },
+  {
+    minimize: false,
+  }
+);
 
 const UserModel = mongoose.model("User", UserSchema);
 module.exports = UserModel;
