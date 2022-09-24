3f8b131bb6e72bc96520d40e77778f3bb72001cf
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-09-24 18:43:04 +0500

    backend is completed for users

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -1,36 +1,10 @@
 const router = require("express").Router();
-const User = require("../../models/UserModel");
+const { loginFunc, signUpFunc } = require("../../controllers/user");
 
 // sign up user
-router.post("/", async (req, res) => {
-  try {
-    const { name, email, password, picture } = req.body;
-    const user = await User.create({ name, email, password, picture });
-    res.status(200).send(user);
-  } catch (error) {
-    let message;
-    if (error.code === 11000) {
-      message = "User is already exists";
-    } else {
-      message = error.message;
-    }
-    res.status(400).send(message);
-  }
-});
+router.post("/", signUpFunc);
 
 // login
-router.post("/login", async (retryWrites) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findByCredentials(email, password);
-    user.status = "online";
-    await user.save();
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-
-//
+router.post("/login", loginFunc);
 
 module.exports = router;
