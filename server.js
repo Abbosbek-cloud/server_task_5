d113febf57c3e442ecdf9e2b9385d1416820aa11
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-09-27 20:26:34 +0500

    edited

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ require("./connection");
 
 const server = require("http").createServer(app);
 
-const PORT = process.env.BASE_URL || 5000;
+const PORT = process.env.PORT || 5000;
 
 const io = require("socket.io")(server, {
   cors: {
